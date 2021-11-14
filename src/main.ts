7e41e324cf573a79da91861ac3d0164cd7c340d3
Alex Long <alexlong2001@outlook.com>
2021-11-14 19:27:22 +0000

    base for select component

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import router from "./router";
 import store from "./store";
 
 import Input from "@/shared/components/input/Input.vue";
+import Select from "@/shared/components/select/Select.vue";
 
 const app = createApp(App).use(store).use(router);
 app.component("Input", Input);
+app.component("Select", Select);
 
 app.mount("#app");
