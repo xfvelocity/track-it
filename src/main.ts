c578e256159ddaa206b6a74eee10582e83c9daa5
Alex Long <alexlong2001@outlook.com>
2021-11-27 10:10:07 +0000

    working manifest & service workers

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,8 @@ import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store/index'
 
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW()
+
 createApp(App).use(store).use(router).use(vuetify).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { VitePWA } from 'vite-plugin-pwa'
 import { join } from 'path'
+
 export default defineConfig({
   mode: 'development',
   resolve: {
@@ -9,7 +10,47 @@ export default defineConfig({
       '@': join(__dirname, './src'),
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Track IT',
+        short_name: 'Track IT',
+        display: 'standalone',
+        background_color: '#0098ff',
+        theme_color: '#0098ff',
+        description: 'TrackIT Gym Tracker',
+        icons: [
+          {
+            src: './img/icons/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: './img/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: './img/icons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: './img/icons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: './img/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   define: { 'process.env': {} },
   css: {
     preprocessorOptions: {
